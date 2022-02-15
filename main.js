bb3195be42a2a5e9f430ad2787875e9460494f60
AugustPeter <Pedro4ugusto14@gmail.com>
2022-02-15 14:12:16 -0300

    Criação das funções Cadastrar,Editar e Show

--- main.js
+++ main.js
@@ -1,5 +1,40 @@
 'use strict'
 
-const openModal = () => document.getElementById('box-show')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//ABRIR E FECHAR A BOXEDIT
+const openBoxEdit = () => document.getElementById('box-edit')
     .classList.add('active')
+const closeBoxEdit = () => document.getElementById('box-edit')
+    .classList.remove('active')
+
+
+//ABRIR E FECHAR A BOXSHOW   
+const openBoxShow = () => document.getElementById('box-show')
+    .classList.add('active')
+const closeBoxShow = () => document.getElementById('box-show')
+    .classList.remove('active')
+
+
+//ABRIR E FECHAR A BOXCADASTRO   
+const openBoxCadastro = () => document.getElementById('box-cadastro')
+    .classList.add('active')
+const closeBoxCadastro = () => document.getElementById('box-cadastro')
+    .classList.remove('active')
+
 
